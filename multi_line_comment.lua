72e0f808d50a8c53a7a2cb169366395c292a82f1
goosecrash <shen.kendrew@gmail.com>
2022-11-16 18:03:36 -0500

    Create multi_line_comment.lua

---
+++ multi_line_comment.lua
@@ -0,0 +1,12 @@
+print("Starting the program")
+
+--[[print("Self destruction in 10...")
+print("9...")
+print("8...")
+print("7...")
+print("6...")
+print("5...")
+print("4...")
+print("3...")
+print("2...")
+print("1...")]]
